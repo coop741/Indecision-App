5000d48353f280fc69c4d4a835e12baf5e67fe42
coop741@coder <5626923+coop741@users.noreply.github.com>
2019-03-25 17:18:27 +0000

    es6 classes exercise part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -16,10 +16,55 @@ class Person {
   }
 }
 
-const me = new Person('John Smith', 22);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age)
+    this.major = major      
+  }
+
+  hasMajor() {
+    return !!this.major
+  }
+
+  getDescription() {
+    let description = super.getDescription()
+    if(this.hasMajor()) {
+      description += ` Their major is ${this.major}.`
+    }
+    return description
+  }
+}
+
+// Class Traveler -> Person
+// Add support for homeLocation
+// Override getGreeting
+// 1. I'm T J. I am visiting from Philly.
+// 2. I'm T J.
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age)
+    this.homeLocation = homeLocation
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting()
+    if(!!this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`
+    }
+    return greeting
+  }
+}
+
+const me = new Student('John Smith', 22, 'Computer Science');
 console.log(me.getGreeting())
 console.log(me.getDescription())
+console.log(me.hasMajor())
 
-const anon = new Person();
+const anon = new Person()
 console.log(anon.getGreeting())
 console.log(anon.getDescription())
+
+const traveler = new Traveler('Anthony Smith', 20, 'Philly')
+console.log(traveler.getGreeting())
+console.log(traveler.getDescription())
